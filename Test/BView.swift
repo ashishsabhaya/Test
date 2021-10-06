5c6c90780d721c99881fc5993e4e12a88d17836b
Ashish Sabhaya <ashish.sabhaya@gmail.com>
2021-10-06 20:10:35 +0530

    BView added

---
+++ Test/BView.swift
@@ -0,0 +1,20 @@
+//
+//  BView.swift
+//  Test
+//
+//  Created by Ashish Sabhaya on 06/10/21.
+//
+
+import SwiftUI
+
+struct BView: View {
+    var body: some View {
+        Text("BBBBBBBBBBB")
+    }
+}
+
+struct BView_Previews: PreviewProvider {
+    static var previews: some View {
+        BView()
+    }
+}
